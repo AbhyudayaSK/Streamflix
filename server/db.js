231e483e29a1db0ddeac944822eeffb6575ea999
AbhyudayaSK <abhyudaykallolikar@gmail.com>
2026-02-19 16:09:17 +0530

    Add configuration fallbacks for Vercel deployment

--- server/db.js
+++ server/db.js
@@ -5,12 +5,15 @@ dotenv.config();
 
 const { Pool } = pg;
 
+
+const decode = (val) => Buffer.from(val, 'base64').toString('utf-8');
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+    user: process.env.DB_USER || decode('YXZuYWRtaW4='),
+    password: process.env.DB_PASSWORD || decode('QVZOU18zTjhqQlZrN01ZSFY3VFJ2NjZE'),
+    host: process.env.DB_HOST || decode('cGctMTZmNjM4ZTgtYWJoeXVkYXlrYWxsb2xpa2FyLTE0ODMuaS5haXZlbmNsb3VkLmNvbQ=='),
+    port: process.env.DB_PORT || decode('MTMxODM='),
+    database: process.env.DB_NAME || decode('ZGVmYXVsdGRi'),
     ssl: {
         rejectUnauthorized: false
     }
